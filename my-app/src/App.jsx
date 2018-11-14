765dbeec9ed2705fcd98cf19ae94d2a4e1e95922
tomaszradwan <radwan.tomasz@gmail.com>
2018-11-14 20:19:14 +0100

    Add Component. Crate class App.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Coundown from './Countdown'
+import React, { Component } from 'react';
+import Countdown from './Countdown';
 
-const App = () => (
-  <div>
-    <Coundown name="śniadanie" time="07:00" />
-    <Coundown name="obiad" time="15:00" />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [
+        { id: 0, name: "śniadanie", time: "07:00" },
+        { id: 1, name: "obiad", time: "15:00" },
+        { id: 2, name: "kolacja", time: "19:00" },
+      ]
+    }
+  }
+
+  render() {
+    const events = this.state.events.map(elem => {
+      return <Countdown key={elem.id}  name={elem.name} time={elem.time} />;
+    });
+    return <div>{events}</div>;
+  }
+}
 
 export default App; 
