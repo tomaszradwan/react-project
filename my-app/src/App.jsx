48913a8651c8438cf226109694eef2a209fe9452
tomaszradwan <radwan.tomasz@gmail.com>
2018-11-18 12:51:02 +0100

    Add EditEvent.jsx, EditEvent.css

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown';
-import "./App.css";
-import "./Countdown.css";
+import EditEvent from './EditEvent';
+import './App.css';
+import './Countdown.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       events: [
-        { id: 0, name: "śniadanie", time: "07:00" },
-        { id: 1, name: "obiad", time: "15:00" },
-        { id: 2, name: "kolacja", time: "19:00" },
+        { id: 0, name: 'śniadanie', time: '07:00' },
+        { id: 1, name: 'obiad', time: '15:00' },
+        { id: 2, name: 'kolacja', time: '19:00' },
       ]
     }
+
+    this.handleEditEvent = this.handleEditEvent.bind(this);
+  }
+
+  handleEditEvent(val) {
+    console.log(val);
   }
 
   render() {
     const events = this.state.events.map(elem => {
-      return <Countdown key={elem.id}  name={elem.name} time={elem.time} />;
+      return <Countdown key={elem.id} name={elem.name} time={elem.time} />;
     });
-    return <div className="app">{events}</div>;
+    return (
+      <div className="app">
+        {events}
+        <EditEvent
+          onInputChange={val => this.handleEditEvent(val)}
+          onSave={() => alert('Test comment!')}
+        />
+      </div>
+    );
   }
 }
 
-export default App; 
+export default App;
