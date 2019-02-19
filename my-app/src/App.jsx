d907192188ee15e45ef7dde5cd3c88f9fead484a
tomaszradwan <radwan.tomasz@gmail.com>
2019-02-19 19:38:37 +0100

    Change componentDidUnmount to componentWillUnmount.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -49,7 +49,7 @@ class App extends Component {
     this.setState({ intervalId: intervalId });
   }
 
-  componentDidUnmount() {
+  componentWillUnmount() {
     clearInterval(this.state.intervalId);
   }
 
